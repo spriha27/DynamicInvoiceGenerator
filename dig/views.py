2f724950d27edddb1066f521ec79729dc9e8086f
spriha27 <spriha27@gmail.com>
2019-06-28 05:16:14 +0530

    Some changes

--- dig/views.py
+++ dig/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 
+# from django.core import mail
+# from django.template.loader import render_to_string
+# from django.utils.html import strip_tags
+
 from dig.models import Document, File
 from dig.forms import DocumentForm, FileForm
 
@@ -18,8 +22,24 @@ def file_upload(request):
         return render(request, 'file_upload.html', {
             'uploaded_file_url': uploaded_file_url
         })
+    else if request.method == 'POST' and request.FILES['mydata']:
+        myfile = request.FILES['mydata']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+        return render(request, 'file_upload.html', {
+            'uploaded_file_url': uploaded_file_url
+        })
     return render(request, 'file_upload.html')
 
-def uploaded_files():
+def continue_to_data(request):
 	return render(request, 'uploaded_files.html')
 
+# def send_mail(request):
+# 	subject = 'Subject'
+# 	html_message = render_to_string('mail_template.html', {'context': 'values'})
+# 	plain_message = strip_tags(html_message)
+# 	from_email = 'From <from@example.com>'
+# 	to = 'to@example.com'
+
+# 	mail.send_mail(subject, plain_message, from_email, [to], html_message=html_message)
